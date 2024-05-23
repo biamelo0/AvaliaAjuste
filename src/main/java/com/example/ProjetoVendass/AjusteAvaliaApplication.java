7b73856757d093c1c7564d103b465254fda1a108
biamelo0 <beatrizmelo0952@gmail.com>
2024-05-23 15:44:45 -0300

    projetoAjuste

---
+++ src/main/java/com/example/ProjetoVendass/AjusteAvaliaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjetoVendass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjusteAvaliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjusteAvaliaApplication.class, args);
+	}
+
+}
